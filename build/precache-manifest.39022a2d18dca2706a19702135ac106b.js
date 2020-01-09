6a90213302d61ba2d5f59891024ddc1c5659bf15
Tissa Khosla <tissa.music@gmail.com>
2020-01-08 22:52:06 -0500

    build branch for new deployment

---
+++ build/precache-manifest.39022a2d18dca2706a19702135ac106b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ccd4f4ac9fcb6546ee45e8ff4dec90",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2f444500cb7ed62d2a2",
+    "url": "/portfolio/static/css/main.71715b0e.chunk.css"
+  },
+  {
+    "revision": "bd2e7ebc9f295ed9c7c2",
+    "url": "/portfolio/static/js/2.c22e408b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.c22e408b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2f444500cb7ed62d2a2",
+    "url": "/portfolio/static/js/main.9f79e451.chunk.js"
+  },
+  {
+    "revision": "1daf2e50e9a968157302",
+    "url": "/portfolio/static/js/runtime-main.5e97d231.js"
+  },
+  {
+    "revision": "50fa2f58d02d97c3d3e582c998cdfbfa",
+    "url": "/portfolio/static/media/Projects.50fa2f58.pdf"
+  },
+  {
+    "revision": "d831ff02af6b548f6e8485441490b9f1",
+    "url": "/portfolio/static/media/TK_Resume.d831ff02.pdf"
+  },
+  {
+    "revision": "dac075653456f5cab9e49b8454e6bd80",
+    "url": "/portfolio/static/media/profilePictureBig.dac07565.png"
+  },
+  {
+    "revision": "9843932fd65a8c28a514e7f89c9e06db",
+    "url": "/portfolio/static/media/projectsScreenshot.9843932f.png"
+  },
+  {
+    "revision": "6c32f0a892ef8726a15635e5e42e34b4",
+    "url": "/portfolio/static/media/writing.6c32f0a8.png"
+  }
+]);
